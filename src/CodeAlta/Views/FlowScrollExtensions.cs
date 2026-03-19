db995ee35c7fff3f1dfc98287799801c39956c39
Alexandre Mutel <alexandre_mutel@live.com>
2026-03-19 07:59:30 +0100

    Disable FollowTail optim as it is missing updates in some cases

--- src/CodeAlta/Views/FlowScrollExtensions.cs
+++ src/CodeAlta/Views/FlowScrollExtensions.cs
@@ -5,7 +5,7 @@ internal static class FlowScrollExtensions
     internal static void ScrollToTailIfFollowing(this DocumentFlow flow)
     {
         ArgumentNullException.ThrowIfNull(flow);
-        if (flow.FollowTail)
+        //if (flow.FollowTail) // We have to disable this for now, as it can happen that adding dynamically elements to the last item in a document flow will not keep the follow tail, and so we are no longer scrolling to the end correctly. This will need to be fixed in the XenoAtom.Terminal.UI
         {
             flow.ScrollToTail();
         }
